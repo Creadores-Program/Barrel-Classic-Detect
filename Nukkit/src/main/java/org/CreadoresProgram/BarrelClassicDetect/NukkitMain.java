3b09371ef3d59fdf4724ba8b4167375bd91dbd03
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-03-24 19:59:09 -0600

    Create NukkitMain.java

---
+++ Nukkit/src/main/java/org/CreadoresProgram/BarrelClassicDetect/NukkitMain.java
@@ -0,0 +1,25 @@
+package org.CreadoresProgram.BarrelClassicDetect;
+import cn.nukkit.Player;
+import cn.nukkit.plugin.PluginBase;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+public class NukkitMain extends PluginBase{
+  private static NukkitMain instance;
+  public static NukkitMain getInstance(){
+    return instance;
+  }
+  public void onEnable(){
+    instance = this;
+    this.getLogger().info("§aDone!");
+  }
+  public void onDisable(){
+    this.getLogger().info("§cBye!");
+  }
+  public boolean isCCPlayer(Player player){
+    return player.getLoginChainData().getDeviceModel().equals("Barrel CREA Classic") && player.getLoginChainData().getDeviceOS() == 7;
+  }
+  public List<Player> getCCPlayers(){
+    return this.getServer().getOnlinePlayers().values().stream().filter(player -> isJavaPlayer(player)).collect(Collectors.toList());
+  }
+}
