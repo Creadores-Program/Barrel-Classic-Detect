7f95692dd460b2e5e26e258efbb9c93f4c3b976d
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-11 08:04:25 -0600

    Refactor NukkitMain class and remove unused listener

--- Nukkit/src/main/java/org/CreadoresProgram/BarrelClassicDetect/NukkitMain.java
+++ Nukkit/src/main/java/org/CreadoresProgram/BarrelClassicDetect/NukkitMain.java
@@ -1,15 +1,12 @@
 package org.CreadoresProgram.BarrelClassicDetect;
 import cn.nukkit.Player;
-import cn.nukkit.event.Listener;
-import cn.nukkit.event.server.DataPacketSendEvent;
-import cn.nukkit.event.EventHandler;
-import cn.nukkit.network.protocol.TextPacket;
 import cn.nukkit.plugin.PluginBase;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
-public class NukkitMain extends PluginBase implements Listener{
+public class NukkitMain extends PluginBase{
   private static NukkitMain instance;
+  private static String deviceModelCC = "Barrel CREA Classic";
   public static NukkitMain getInstance(){
     return instance;
   }
@@ -21,25 +18,9 @@ public class NukkitMain extends PluginBase implements Listener{
     this.getLogger().info("§cBye!");
   }
   public boolean isCCPlayer(Player player){
-    return player.getLoginChainData().getDeviceModel().contains("Barrel CREA Classic") && player.getLoginChainData().getDeviceOS() == 7;
+    return player.getLoginChainData().getDeviceModel().contains(deviceModelCC) && player.getLoginChainData().getDeviceOS() == 7;
   }
   public List<Player> getCCPlayers(){
     return this.getServer().getOnlinePlayers().values().stream().filter(player -> isCCPlayer(player)).collect(Collectors.toList());
-  }
-  @EventHandler
-  public void onDataPacketSendEvent(DataPacketSendEvent event){
-    Player player = event.getPlayer();
-    if(getServer().isLanguageForced() || player == null || !(event.getPacket() instanceof TextPacket) || !this.isCCPlayer(player) || event.isCancelled()){
-      return;
-    }
-    TextPacket packet = (TextPacket) event.getPacket();
-    if(packet.type != TextPacket.TYPE_TRANSLATION){
-      return;
-    }
-    event.setCancelled(true);
-    TextPacket pk = new TextPacket();
-    pk.type = TextPacket.TYPE_RAW;
-    pk.message = getServer().getLanguage().translateString(packet.message, packet.parameters);
-    player.dataPacket(pk);
   }
 }
