0715e4dc2da95ab4438aeb9f74fae0eb37e812df
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-11 08:08:38 -0600

    Refactor device model check and remove unused event

--- Pocketmine/plugin/src/org/CreadoresProgram/BarrelClassicDetect/PocketmineMain.php
+++ Pocketmine/plugin/src/org/CreadoresProgram/BarrelClassicDetect/PocketmineMain.php
@@ -2,12 +2,10 @@
 declare(strict_types=1);
 namespace org\CreadoresProgram\BarrelClassicDetect;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\server\DataPacketSendEvent;
-use pocketmine\network\mcpe\protocol\TextPacket;
 use pocketmine\Player;
 class PocketmineMain extends PluginBase{
   private static ?self $instance = null;
+  private static string $deviceModelCC = "Barrel CREA Classic";
   public static function getInstance() : ?self{
     return self::$instance;
   }
@@ -18,31 +16,9 @@ class PocketmineMain extends PluginBase{
     $this->getLogger()->info("§cBye!");
   }
   public function isCCPlayer(Player $player) : bool{
-    return ($player->getDeviceModel() == "Barrel CREA Classic") && ($player->getDeviceOS() == 7);
+    return ($player->getDeviceModel() == self::$deviceModelCC) && ($player->getDeviceOS() == 7);
   }
   public function getCCPlayers() : array{
     return array_filter($this->getServer()->getOnlinePlayers(), fn($player) => $this->isCCPlayer($player));
-  }
-  public function onDataPacketSend(DataPacketSendEvent $event) : void {
-    $player = $event->getPlayer();
-    $packet = $event->getPacket();
-
-    if(!$packet instanceof TextPacket){
-        return;
-    }
-    if($player === null || !$this->isCCPlayer($player)){
-        return;
-    }
-    if($packet->type !== TextPacket::TYPE_TRANSLATION){
-        return;
-    }
-    $event->setCancelled(true);
-
-    $newPacket = new TextPacket();
-    $newPacket->type = TextPacket::TYPE_RAW;
-    
-    $newPacket->message = $this->getServer()->getLanguage()->translateString($packet->message, $packet->parameters);
-    
-    $player->sendDataPacket($newPacket);
   }
 }
